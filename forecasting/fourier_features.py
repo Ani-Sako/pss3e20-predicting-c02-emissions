83a69d6510089d134f43ec4ae6326306b9d3d349
Ani-Sako <animesh.sakorikar@gmail.com>
2023-08-21 09:26:40 -0700

    Extracted fourier feature generation functionality

---
+++ forecasting/fourier_features.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+
+def fourier_features(time_dummy, fourier_feature_frequencies, time_dummy_frequency=1):
+    time = np.array(time_dummy, dtype=np.float32)
+    fourier_feature_dict = {}
+    for frequency in fourier_feature_frequencies:
+        theta = 2*np.pi*(time_dummy_frequency/frequency)
+        fourier_feature_dict.update({f'sin_{frequency}': np.sin(theta),
+                                     f'cos_{frequency}': np.cos(theta)})
+
+    return pd.DataFrame(fourier_feature_dict, index=time_dummy)
