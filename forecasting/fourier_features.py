4eb938e3b9de672853a7caa8eabc76f5fd770f20
Ani-Sako <animesh.sakorikar@gmail.com>
2023-08-22 08:36:11 -0700

    Fix minor bugs

--- forecasting/fourier_features.py
+++ forecasting/fourier_features.py
@@ -6,7 +6,7 @@ def fourier_features(time_dummy, fourier_feature_frequencies, time_dummy_frequen
     time = np.array(time_dummy, dtype=np.float32)
     fourier_feature_dict = {}
     for frequency in fourier_feature_frequencies:
-        theta = 2*np.pi*(time_dummy_frequency/frequency)
+        theta = 2*np.pi*(frequency/time_dummy_frequency)*time
         fourier_feature_dict.update({f'sin_{frequency}': np.sin(theta),
                                      f'cos_{frequency}': np.cos(theta)})
 
--- forecasting/prepare_forecast_dataset.py
+++ forecasting/prepare_forecast_dataset.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 
 def prepare_forecast_dataset(timeseries, forecast_horizon, lag_features, lead_time=0):
-    x = pd.concat({f'step_{steps}': timeseries.shift(steps + lead_time)
-                   for steps in range(1, lag_features)},
+    x = pd.concat({f'lag_{lag}': timeseries.shift(lag + lead_time)
+                   for lag in range(1, lag_features+1)},
                   axis=1)
     y = pd.concat({f'step_{steps}': timeseries.shift(-steps)
-                   for steps in range(0, forecast_horizon-1)},
+                   for steps in range(0, forecast_horizon)},
                   axis=1)
     return x, y
