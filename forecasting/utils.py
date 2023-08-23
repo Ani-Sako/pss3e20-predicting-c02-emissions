a94d23155054939ff8b82713f8c10d1e9ec8d736
Ani-Sako <animesh.sakorikar@gmail.com>
2023-08-23 02:02:41 -0700

    Moved PCA and data cleaning to utils

---
+++ forecasting/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+
+
+def pca_timeseries(data, num_componenets):
+    pca = PCA(n_components=num_componenets)
+    pca.fit(data)
+    dfpca = pca.transform(data)
+    return pca, pd.DataFrame(dfpca, columns=['ts'+str(i+1) for i in range(num_componenets)])
+
+
+def remove_nulldata(x_data, y_data):
+    null_indices = pd.concat([x_data, y_data], axis=1).isna().any(axis=1)
+    valid_indices = null_indices[null_indices == False].index
+    x_train_residual = x_data.loc[valid_indices, :]
+    y_train_residual = y_data.loc[valid_indices, :]
+    return  x_train_residual, y_train_residual
